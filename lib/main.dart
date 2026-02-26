40aadeb067eeef800a1e10b3c7056e832141f98a
Your Name <your.email@example.com>
2026-02-26 09:25:55 +0800

    hive commit message "fix: remove debug banner and set home page to HomePage()"

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
 
 
-void main() {
+void main() async {
+  // initialize hive
+  await Hive.initFlutter();
+ // open a box
+  await Hive.openBox('myBox');
+
+
+
   runApp(MyApp());
 }
 
