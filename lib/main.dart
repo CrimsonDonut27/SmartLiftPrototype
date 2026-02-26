1647c232de72bb0d7af37a1bc1f1d92012232ef2
Your Name <your.email@example.com>
2026-02-26 08:55:19 +0800

    sa seperate na file na ang homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
+
 
 void main() {
   runApp(MyApp());
 }
+
+
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,54 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 0, 255, 55),
-
-          title: const Text('PAKYU NIGGER'),
-        ),
-
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('Floating Action Button Pressed');
-          },
-          child: const Icon(Icons.add),
-        ) ,
-
-
-
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment
-              .center, //MainAxisAlignment.center, - Spacing between the children along the main axis (vertical in this case).
-          crossAxisAlignment: CrossAxisAlignment
-              .end, //CrossAxisAlignment.center, - Alignment of the children along the cross axis (horizontal in
-
-          children: const [
-            Icon(Icons.bar_chart),
-            Icon(Icons.battery_charging_full),
-            Icon(Icons.brightness_5),
-          ],
-        ),
-
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-      ),
-
-      drawer: Drawer(),
-    ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
 
   }
