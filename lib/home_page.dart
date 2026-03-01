beb2215c95a4d440b0cbdd88b72357f970311dc7
Your Name <your.email@example.com>
2026-03-01 17:18:09 +0800

    Yung main code napunta sa home_page nagdagdag ako ng floating action button para mag add ng exercise nag add din ako ng function para mag open ng dialog box kapag na click yung button nag add din ako ng import ng hive_flutter para magamit ko yung database sa pag save ng exercise data nag add ako dalawang function para mag add at mag delete ng exercise pero wala pa silang laman sa home page pa nagdidisplay ang mga saved na exercise data pero wala pa akong code para mag display ng data sa UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,8 +10,94 @@ class HomePage extends StatefulWidget {
 
 
 class _HomePageState extends State<HomePage> {
+
+  // get the box
+  final _myBox = Hive.box('myBox');
+
+  // text controller
+  final _textController = TextEditingController();
+
+  // list of exercises
+  List<String> exercises = [];
+
+
+  @override
+  void initState() {
+    // load data, if none, empty list
+    exercises = _myBox.get("Excercise_List") ?? [];
+
+    super.initState();
+  }
+
+  // open new exercise dialog
+  void openNewExercise() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Add New Exercise"),
+        content: TextField(
+          controller: _textController,
+          decoration: const InputDecoration(hintText: "Exercise Name"),
+        ),
+
+        actions: [
+
+          //cancel button
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              _textController.clear();
+            }, 
+            child: const Text("Cancel")
+            ),
+
+          //add button
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              addNewExercise();
+            }, 
+            child: const Text("Add")
+          )
+        ],
+      )
+    );
+  }
+
+  // add new exercise
+  void addNewExercise() {
+    String exerciseName = _textController.text;
+    setState(() {
+      exercises.add(exerciseName);
+      _textController.clear();
+
+    });
+
+  }
+  // delete exercise
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      // add exercise button
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: openNewExercise,
+        label: const Text("Add Exercise"),
+        icon: const Icon(Icons.add),
+      ),
+
+
+      // show list of exercises
+        body: ListView.builder(
+          itemCount: exercises.length,
+          itemBuilder: (context, index) {
+            // get each exercise
+            final exercise = exercises[index];
+            return ListTile(
+              title: Text(exercise),
+            );
+          },
+        ),
+    );
   }
 }
